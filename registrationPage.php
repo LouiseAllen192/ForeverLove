ef6198aee14e057df86b10df5ea9603cbfb5e052
kfcobrien <kfcobrien@gmail.com>
2016-03-10 12:49:01 +0000

    Fighting with css

--- registrationPage.php
+++ registrationPage.php
@@ -5,11 +5,13 @@
     include("includes/metatags.html");
     ?>
 
-    <title>Register Prefrences</title>
+    <title>Registration Page</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/custom-base-page.css" rel="stylesheet">
     <link href="css/custom-form-page.css" rel="stylesheet">
     <?php include("includes/fonts.html"); ?>
+
+
 </head>
 
 <body class="full">
@@ -24,31 +26,71 @@
                 <br><br>
                 <h2>
                     <small>
-                        <strong>New Registration</strong>
+                        <strong>Please Complete Registration</strong>
                     </small>
                 </h2>
                 <hr class="tagline-divider">
                 <p>
                     <br>
 
-                <form role="form" method="post">
-                    <label for="Email" class="h5"><b>Email Address:</b></label>
-                    <input type="email" class="form-control" name="Email"  value="<?php echo Input::get('Email');?>" autocomplete="on">
+                <form name="regForm" class="form-horizontal" role="form" method="post">
+                    <fieldset>
+                        <div class="form-group">
+                            <label for="Email" class="col-md-4 control-label"><b>Email</b></label>
+                            <div class="col-md-7">
+                                <input type="email" class="form-control" name="Email"  value="">
+                            </div>
+                            <input class="col-md-1" type="checkbox" disabled name="Valid_Email">
+                        </div>
 
-                    <label for="Username" class="h5"><b>Username:</b></label>
-                    <input type="text" class="form-control" name="Username"  value="<?php echo Input::get('Username');?>">
+                        <div class="form-group">
+                            <label for="Confirm_Email" class="col-md-4 control-label"><b>Confirm Email</b></label>
+                            <div class="col-md-7">
+                                <input type="email" class="form-control" name="Confirm_Email"  value="">
+                            </div>
+                            <input class="col-md-1" type="checkbox" disabled name="Valid_Confirm_Email">
+                        </div>
 
-                    <label for="First_Name" class="h5"><b>First Name:</b></label>
-                    <input type="text" class="form-control" name="First_Name" value="<?php echo Input::get('First_Name');?>" autocomplete="on">
+                        <div class="form-group">
+                            <label for="Username" class="col-md-4 control-label"><b>Username</b></label>
+                            <div class="col-md-7">
+                                <input type="text" class="form-control" name="Username"  value="<?php echo Input::get('Username');?>">
+                            </div>
+                            <input class="col-md-1" type="checkbox" disabled name="Valid_Username">
+                        </div>
 
-                    <label for="Last_Name" class="h5"><b>Last Name:</b></label>
-                    <input type="text" class="form-control" name="Last_Name" value="<?php echo Input::get('Last_Name');?>" autocomplete="on">
+                        <div class="form-group">
+                            <label for="First_Name" class="col-md-4 control-label"><b>First Name</b></label>
+                            <div class="col-md-7">
+                                <input type="text" class="form-control" name="First_Name" value="<?php echo Input::get('First_Name');?>" autocomplete="on">
+                            </div>
+                            <input class="col-md-1" type="checkbox" disabled name="Valid_First_Name">
+                        </div>
 
-                    <label for="Password" class="h5"><b>Password:</b></label>
-                    <input type="password" class="form-control" name="Password" value="">
+                        <div class="form-group">
+                            <label for="Last_Name" class="col-md-4 control-label"><b>Last Name</b></label>
+                            <div class="col-md-7">
+                                <input type="text" class="form-control" name="Last_Name" value="<?php echo Input::get('Last_Name');?>" autocomplete="on">
+                            </div>
+                            <input class="col-md-1" type="checkbox" disabled name="Valid_Last_Name">
+                        </div>
 
-                    <label for="Confirm_Password" class="h5"><b>Confirm Password:</b></label>
-                    <input type="password" class="form-control" name="Confirm_Password" value="">
+                        <div class="form-group">
+                            <label for="Password" class="col-md-4 control-label"><b>Password</b></label>
+                            <div class="col-md-7">
+                                <input type="password" class="form-control" name="Password" value="">
+                            </div>
+                            <input class="col-md-1" type="checkbox" disabled name="Valid_Password">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="Confirm_Password" class="col-md-4 control-label"><b>Confirm Password</b></label>
+                            <div class="col-md-7">
+                                <input type="password" class="form-control" name="Confirm_Password" value="">
+                            </div>
+                            <input class="col-md-1" type="checkbox" disabled name="Valid_Confirm_Password">
+                        </div>
+                    </fieldset>
 
                     <br><br>
                     <a href="welcomePage.php" class="btn btn-info center-inline" role="button">Back</a>
@@ -60,8 +102,21 @@
             </div>
         </div>
     </div>
-
 </div>
+<script type="text/javascript">
+    function checkForm(){
+        if(this.Email.value == "" || !this.Valid_Email.checked){
+            alert("Please enter a valid Email address.")
+            this.Email.focus();
+            return false;
+        }
+        if(this.Confirm_Email.value != this.Email.value || !this.Valid_Confirm_Email.checked){
+            alert("Email addresses do not match");
+            this.Confirm_Email.focus();
+            return false;
+        }
+    }
+</script>
 <?php include("includes/footer.html"); ?>
 </body>
 
