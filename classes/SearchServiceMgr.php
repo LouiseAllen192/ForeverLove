2a56ca8a3f7c2718324234f0b066976dd807b3be
kfcobrien <kfcobrien@gmail.com>
2016-03-24 02:51:47 +0000

    Updated searchPage functionality

--- classes/SearchServiceMgr.php
+++ classes/SearchServiceMgr.php
@@ -2,49 +2,44 @@
 class SearchServiceMgr{
 
     public static function byCriteria($list, $preferences){
-        $sql = "SELECT a.user_id,a.username,a.tag_line,a.city,Total FROM (SELECT z.user_id,z.username,z.tag_line,z.city";
+        $sql = "SELECT d.user_id,d.username,d.tag_line,d.city,Total FROM (SELECT c.user_id,c.username,c.tag_line,c.city,";
         $n = 1;
         $last = count($preferences);
         foreach($preferences as $preference => $value){
-            if($n <= $last){
-                $sql .= ",";
-            }
-            if($n == $last){
-                $sql .= "(CASE WHEN z.$preference IS NOT NULL && z.$preference = $value THEN z.Total + 1 ELSE z.Total END)AS Total";
+            if($n++ == $last){
+                $sql .= "(CASE WHEN c.$preference IS NOT NULL && c.$preference = $value THEN c.Total + 1 ELSE c.Total END)AS Total";
             }
             else{
-                $sql .= "(CASE WHEN z.$preference IS NOT NULL && z.$preference = $value THEN 1 ELSE 0 END)+";
+                $sql .= "(CASE WHEN c.$preference IS NOT NULL && c.$preference = $value THEN 1 ELSE 0 END)+";
             }
         }
-        if(!$last) $sql .= ",Total";
-        $sql .= " FROM (SELECT y.*,";
+        if(!$last) $sql .= "Total";
+        $sql .= " FROM (SELECT b.*,";
         $n = 1;
         $last = count($list);
         foreach($list as $item){
-            $sql .= "COALESCE(y.$item, 0)";
+            $sql .= "COALESCE(b.$item, 0)";
             if($n++ < $last){
                 $sql .= "+";
             }
         }
         if(!$last) $sql .= "0";
-        $sql .= " AS Total FROM (SELECT x.user_id,x.username,x.tag_line,x.city";
+        $sql .= " AS Total FROM (SELECT a.user_id,a.username,a.tag_line,a.city";
         $n = 1;
         $last = count($preferences);
-        $requiredPreferences = '';
         foreach($preferences as $preference => $value){
             if($n++ <= $last){
                 $sql .= ",";
             }
-            $requiredPreferences .= 'x.'.$preference;
+            $sql .= 'a.'.$preference;
         }
-        $sql .= $requiredPreferences;
         $n = 1;
         $last = count($list);
         foreach($list as $item){
             if($n++ <= $last){
                 $sql .= ",";
             }
-            $sql .= "MAX(CASE WHEN x.hobby_id = '$item' THEN x.hobby_preference END)AS '$item'";
+            $sql .= "MAX(CASE WHEN a.hobby_id = '$item' THEN a.hobby_preference END)AS '$item'";
         }
         $n = 1;
         $last = count($preferences);
@@ -57,7 +52,7 @@ class SearchServiceMgr{
         }
         $sql .= " FROM (SELECT user_id,username,tag_line,city,hobby_id,hobby_preference$requiredPreferences";
         $sql .= " FROM registration_details JOIN preference_details USING(user_id)";
-        $sql .= " JOIN user_hobby_preferences USING(user_id))x GROUP BY user_id)y )z ORDER BY Total DESC)a WHERE Total > 0";
+        $sql .= " JOIN user_hobby_preferences USING(user_id))a GROUP BY user_id)b )c ORDER BY Total DESC)d WHERE Total > 0";
         return DB::getInstance()->query($sql)->results();
     }
 }
