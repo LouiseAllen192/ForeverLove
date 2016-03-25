2ba5dda4de3241b5fac85162be5d4cf8553520ab
kfcobrien <kfcobrien@gmail.com>
2016-03-25 00:09:52 +0000

    Added viewAll page

--- viewAllPage.php
+++ viewAllPage.php
@@ -6,11 +6,17 @@
     <?php
     require_once 'core/init.php';
     include("includes/metatags.html");
+    include("includes/fonts.html");
+
+    $_SESSION['user_id'] = 1;//DELETE
+
+    $uid = $_SESSION['user_id'];
+    $sql = "SELECT user_id,username,tag_line,city,gender,seeking FROM registration_details JOIN preference_details USING(user_id) WHERE user_id != $uid";
+    $results = SearchServiceMgr::filterSeekingGender($uid, DB::getInstance()->query($sql)->results());
     ?>
     <title>View All Page</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/custom-base-page.css" rel="stylesheet">
-    <?php include("includes/fonts.html"); ?>
 
 </head>
 
@@ -30,18 +36,37 @@
                     </small>
                 </h2>
                 <hr class="tagline-divider">
-                <p>
-
+                <br><br>
+                    <?php
+                    if(isset($results)){
+                        foreach ($results as $result){
+                            ?>
+                            <div class="display_box">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <div class="media">
+                                            <div class="media-left">
+                                                <img class="media-object" src="https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/73.jpg"/>
+                                            </div>
+                                            <div class="media-body" style="padding-top: 3px;">
+                                                <h4 class="media-heading"><?php echo $result->username; ?></h4>
+                                                <small style="white-space: nowrap;"><?php echo $result->tag_line; ?></small>
+                                            </div>
+                                            <div class="media-right media-middle">
+                                                <h5 class="media-heading"><?php echo $result->city; ?></h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                    }?>
                     <br><br>
-                    Insert view all page contents here
-
                     <br><br>
-                    <br><br>
-                </p>
             </div>
         </div>
     </div>
-
 </div>
 <?php include("includes/footer.html"); ?>
 </body>
