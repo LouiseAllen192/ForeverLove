e4c42b15aadab4a8d038d2662a66079f21e80683
Louise Allen <louise.allen192@gmail.com>
2016-03-27 20:32:38 +0100

    added delete picture functionality and update profile picture functionality

--- classes/ImageService.php
+++ classes/ImageService.php
@@ -16,33 +16,55 @@ class ImageService
         return $resultFinal;
     }
 
-    public static function updateProfileImage($imageNum, $uid){
-        //todo
-        return true;
+    public static function updateProfileImage($imgNum, $uid){
+        $sql1 = "SELECT * " .
+            "FROM images  ".
+            "WHERE user_id = '".$uid."' AND image_id = '".$imgNum."'";
+        $newResults = DB::getInstance()->query($sql1)->results()[0];
+        $newProfileName = $newResults->image_name;
+        $newProfilePath  = $newResults->image_path;
+
+        $sql2 = "SELECT * " .
+            "FROM images  ".
+            "WHERE user_id = '".$uid."' AND image_id = 1";
+        $oldResults = DB::getInstance()->query($sql2)->results()[0];
+        $oldProfileName = $oldResults->image_name;
+        $oldProfilePath  = $oldResults->image_path;
+
+        $changesProfile = array("image_path" => "$newProfilePath", "image_name" => "$newProfileName");
+        $whereProfile = "user_id = '".$uid."' AND image_id = 1";
+        $successProfile = DB::getInstance()->update('images', $whereProfile , $changesProfile);
+
+
+        $changesOther = array("image_path" => "$oldProfilePath", "image_name" => "$oldProfileName");
+        $whereOther =  "user_id = '".$uid."' AND image_id = '".$imgNum."'";
+        $successOther = DB::getInstance()->update('images', $whereOther , $changesOther);
+
+
+        if($successProfile && $successOther){
+            return true;
+        }
+        return false;
+
     }
 
 
     public static function deleteImage($imgNum, $uid){
         $sql = "SELECT image_name " .
             "FROM images  ".
-            "WHERE user_id = '".$uid."' AND image_id = '".$imgNum."'\";" ;
+            "WHERE user_id = '".$uid."' AND image_id = '".$imgNum."'";
         $results = DB::getInstance()->query($sql)->results()[0];
+        $imgName = $results->image_name;
 
-        foreach ($results as $result) {
-           $imgName = $result->image_name;
+        $path = "userImageUploads/user".$uid."/".$imgName;
+        unlink($path);
+
+        $update = array("image_path" => "", "image_name" => "");
+        $where = "user_id = '".$uid."' AND image_id = '".$imgNum."'";
+        if(DB::getInstance()->update('images', $where , $update)){
+            return true;
         }
-
-//        $path = "userImageUploads/user".$uid."/".$imgName;
-//        unlink($path);
-
-//        $update = array("image_path" => "", "image_name" => "");
-//        $where = "user_id = '".$uid."' AND image_id = '".$imgNum."'";
-//        if(DB::getInstance()->update('images', $where , $update)){
-//            return true;
-//        }
-        //return false;
-
-        return true;
+        return false;
     }
 
     public static function uploadImage($uid, $imgNum, $path, $name){
