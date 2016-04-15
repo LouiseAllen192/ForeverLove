b5547b91cc7a1b0d4650c695f03f9f24dd295a94
Louise Allen <louise.allen192@gmail.com>
2016-04-15 20:57:08 +0100

    fixed css on settings and home page

--- homePage.php
+++ homePage.php
@@ -20,6 +20,7 @@
     <div class="container">
 
         <div class = "box">
+
         <div class="row">
 
             <div class="col-lg-1 col-sm-4"></div>
--- settingsPage.php
+++ settingsPage.php
@@ -9,7 +9,7 @@
     ?>
     <title>Settings Page</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/custom-base-page.css" rel="stylesheet">
+    <link href="css/custom-settings.css" rel="stylesheet">
     <?php include("includes/fonts.html"); ?>
 
 </head>
@@ -23,35 +23,68 @@
     <div class="row">
         <div class="box">
             <div class="col-lg-12 text-center">
-                <br><br>
                 <h2>
                     <small>
                         <strong>Settings</strong>
                     </small>
                 </h2>
                 <hr class="tagline-divider">
-                <p><br>
+                <p>
+                        <br>
 
-                <div class = "panel panel-default">
-                    <div class = "panel-body">
 
-                        <br><br>
-                        <a href="viewMembershipStatusPage.php">View membership status</a><br><br>
-                        <a href="updateRegDetailsPage.php">Update your basic account details</a><br><br>
-                        <a href="updatePassword.php">Update your password</a><br><br>
-                        <a href="updatePreferencesPage.php">Update your prefrences</a><br><br>
-                        <a href="updateHobbiesPage.php">Update your hobbies</a><br><br>
+                        <div class="row">
 
-                        <br><br>
-                        <br><br>
+                            <div class="col-lg-1 col-sm-4"></div>
+                            <div class = "col-lg-10 col-sm-10">
+                                <div class="col-lg-4 col-sm-6 text-center">
+                                    <a href ="profilePage.php"><img class="img-circle img-responsive img-center" src="http://gdurl.com/X2ql" alt="Profile Icon"></a>
+                                    <a href="viewMembershipStatusPage.php"><h4>View membership status</h4></a>
+                                    <p><br></p>
+                                </div>
+                                <div class="col-lg-4 col-sm-6 text-center">
+                                    <a href = "messagesPage.php"><img class="img-circle img-responsive img-center" src="http://gdurl.com/jBkk" alt="Messages Icon"></a>
+                                    <a href="updateRegDetailsPage.php"><h4>Update your basic account details</h4></a>
+                                    <p><br></p>
+                                </div>
+                                <div class="col-lg-4 col-sm-6 text-center">
+                                    <a href = "searchPage.php"><img class="img-circle img-responsive img-center" src="http://gdurl.com/CRJI" alt="Search Icon"></a>
+                                    <a href="updatePassword.php"><h4>Update your password</h4></a><br><br>
+                                    <p><br></p>
+                                </div>
+                            </div>
+                            <div class="col-lg-1 col-sm-4"></div>
+                        </div>
+
+                        <div class="row"></div>
+
+                        <div class="row">
+                            <div class="col-lg-1 col-sm-4"></div>
+                            <div class = "col-lg-10 col-sm-10">
+                                <div class="col-lg-4 col-sm-9 text-center">
+                                    <a href = "suggestionsPage.php"><img class="img-circle img-responsive img-center" src="http://gdurl.com/iAsL" alt="Suggestions Icon"></a>
+                                    <a href="updatePreferencesPage.php"><h4>Update your prefrences</h4></a>
+                                    <p><br></p>
+                                </div>
+                                <div class="col-lg-4 col-sm-9 text-center">
+                                    <a href = "viewAllPage.php"><img class="img-circle img-responsive img-center" src="http://gdurl.com/UNQK" alt="View All Icon"></a>
+                                    <a href="updateHobbiesPage.php"><h4>Update your hobbies</h4></a>
+                                    <p><br></p>
+                                </div>
+                                <div class="col-lg-4 col-sm-9 text-center">
+                                    <a href = "viewAllPage.php"><img class="img-circle img-responsive img-center" src="http://gdurl.com/nyDn" alt="View All Icon"></a>
+                                    <a href="contactPage.php"><h4>View ForeverLove Contact Details</h4></a>
+                                    <p><br></p>
+                                </div>
+                            </div>
+                            <div class="col-lg-1 col-sm-4"></div>
+                        </div>
 
                     </div>
                 </div>
 
                 </p>
             </div>
-        </div>
-    </div>
 
 </div>
 <?php include("includes/footer.html"); ?>
